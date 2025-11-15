fdd2dddd48af42576a76c4ac381ae9d4382673e8
Simone Remoli <simoneremoli00@gmail.com>
2025-11-15 19:29:52 +0100

    Decorator

--- src/Decorator/Main.java
+++ src/Decorator/Main.java
@@ -3,7 +3,25 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Component dressed = new TShirt(new Underwear(new Eyeglasses(new Shoes(new Man("Guglielmo")))));
+        Man a = new Man("John");
+        Shoes s = new Shoes(a);
+        Eyeglasses e = new Eyeglasses(s);
+        Underwear u = new Underwear(e);
+        TShirt t = new TShirt(u);
+        //t.incNum();
+        Component c = t;
+        System.out.println(c.dress_code());
+
+
+
+
+
+
+        /*Component dressed = new TShirt(new Underwear(new Eyeglasses(new Shoes(new Man("Guglielmo")))));
         System.out.println(dressed.dress_code());
+        Component dressed2 = new Underwear(new TShirt(new Shoes(new Eyeglasses(new Man("Guglielmo")))));
+        System.out.println(dressed2.dress_code());
+        */
+
     }
 }
