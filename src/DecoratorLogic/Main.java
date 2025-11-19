f7c701f8dad5ce16dfdf84f1bb3f1b866717af8b
Simone Remoli <simoneremoli00@gmail.com>
2025-11-19 14:58:54 +0100

    Decorator

--- src/DecoratorLogic/Main.java
+++ src/DecoratorLogic/Main.java
@@ -13,6 +13,10 @@ public class Main {
 
         DressCode result = c.dress_code(); //cambia il result in base alle chiamate
 
+        /*
+        Si manipola l'oggetto DressCode in base alla logica di ogni decorator.
+        Alla fine si ottiene un DressCode che riflette tutte le modifiche apportate.
+         */
         System.out.println("Style: " + result.styleLevel);
         System.out.println("Formal: " + result.isFormal);
 
